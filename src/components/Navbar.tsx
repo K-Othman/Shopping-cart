e92798eb0e94c88693857e5c3e1e30424bd0c0d7
K-Othman <karim_othman88@hotmail.com>
2023-02-08 01:15:33 +0000

    setting up the project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Container, Navbar as NavbarBs } from "react-bootstrap";
+
+export function Navbar() {
+  return (
+    <NavbarBs className="bg-white shadow-sm wb-3">
+      <Container>Nav</Container>
+    </NavbarBs>
+  );
+}
